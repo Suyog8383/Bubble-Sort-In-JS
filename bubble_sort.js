754a8e519501be928450e43e237aede4a8d8afeb
Suyog Kalyan Nagawade <92072200+Suyog8383@users.noreply.github.com>
2023-06-20 21:33:27 +0530

    Create bubble_sort.js

---
+++ bubble_sort.js
@@ -0,0 +1,11 @@
+let arr=[8,1,7,2,5];
+for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr.length;j++){
+        if(arr[j]>arr[j+1]){
+            let temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+        }
+    }
+}
+console.log(arr);
